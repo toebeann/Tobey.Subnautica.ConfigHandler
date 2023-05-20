78df3ab42fee1154632782f042c8e50d2d9e0366
Tobey Blaber <contact@tobey.me>
2023-05-20 11:51:51 +0100

    Small performance optimisation

--- Configuration/BepInExExtensionMethods.cs
+++ Configuration/BepInExExtensionMethods.cs
@@ -1,18 +1,14 @@
 ﻿using BepInEx.Configuration;
-using System.Linq;
 
 namespace Tobey.Subnautica.ConfigHandler.Configuration;
 internal static class BepInExExtensionMethods
 {
-    public static ConfigEntry<T> GetConfigEntry<T>(this ConfigFile configFile, string section, string key) =>
-            configFile.FirstOrDefault(kvp =>
-                kvp.Key.Section == section &&
-                kvp.Key.Key == key)
-            .Value switch
-            {
-                ConfigEntry<T> entry => entry,
-                _ => null
-            };
+    public static ConfigEntry<T> GetConfigEntry<T>(this ConfigFile configFile, string section, string key)
+        => configFile?[section, key] switch
+        {
+            ConfigEntry<T> entry => entry,
+            _ => null
+        };
 
     public static ConfigEntry<T> Bind<T>(this ConfigFile configFile, Definition<T> args) => configFile.Bind(
         section: args.Section,
