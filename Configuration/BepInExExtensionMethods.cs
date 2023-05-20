01e8ada43490d0a59c368d465c54fe881020b493
Tobey Blaber <contact@tobey.me>
2023-05-20 13:08:09 +0100

    Fix formatting

--- Configuration/BepInExExtensionMethods.cs
+++ Configuration/BepInExExtensionMethods.cs
@@ -3,11 +3,11 @@
 namespace Tobey.Subnautica.ConfigHandler.Configuration;
 internal static class BepInExExtensionMethods
 {
-public static ConfigEntry<T> Bind<T>(this ConfigFile configFile, Definition<T> args) => configFile.Bind(
-        section: args.Section,
-        key: args.Key,
-        configDescription: new(
-            description: args.Description,
-            tags: args.Tags),
-        defaultValue: args.DefaultValue);
+    public static ConfigEntry<T> Bind<T>(this ConfigFile configFile, Definition<T> args) => configFile.Bind(
+            section: args.Section,
+            key: args.Key,
+            configDescription: new(
+                description: args.Description,
+                tags: args.Tags),
+            defaultValue: args.DefaultValue);
 }
